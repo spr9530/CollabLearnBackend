858483b1007a0bd2403e4d5dc6a7636c7567ada6
spr9530 <145482692+spr9530@users.noreply.github.com>
2024-06-30 19:36:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-    origin: "*",
+    origin: "http://localhost:5173",
     allowedHeaders: ["my-custom-header"],
     credentials: true
   }
