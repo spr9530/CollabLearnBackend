248a54df7dba9e3b14110e0c7afa5c99919d3d4d
spr9530 <145482692+spr9530@users.noreply.github.com>
2024-06-30 19:51:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,15 +14,11 @@ const server = http.createServer(app);
 app.use(cors());
 
 const io = new Server(server, {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
-    }
+    cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 });
 
 const peerServer = ExpressPeerServer(server, {
