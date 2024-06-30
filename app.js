87714a0fb8efb95c6aab53f094e46d7e709a266e
spr9530 <145482692+spr9530@users.noreply.github.com>
2024-06-30 18:46:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: '*'
+        origin: '*',
     }
 });
 const peerServer = ExpressPeerServer(server, {
