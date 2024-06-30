f0c1692bbfb78d2eb70ffebf748bd366cccfc60a
spr9530 <145482692+spr9530@users.noreply.github.com>
2024-06-30 19:44:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,10 +16,15 @@ connectDB()
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-    origin: "*",
-    credentials: true
-  }
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
 });
 const peerServer = ExpressPeerServer(server, {
     debug: true,
