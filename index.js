9a54ac50cb00ef30dcd705655bbb3924f6498177
spr9530 <145482692+spr9530@users.noreply.github.com>
2024-06-30 15:53:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connectDB();
 
 const app = express();
 const server = http.createServer(app);
-app.use(cors();
+app.use(cors());
 const io = new Server(server, {
     cors: {
         origin: 'https://qwertyuioplkjhgfdsaqwertyuiop.netlify.app',
